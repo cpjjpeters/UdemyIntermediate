a2b443f9f2639d335debf4202095d35cb1ec08f9
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2017-12-10 17:55:14 +0100

    Add project files.

---
+++ UdemyIntermediate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.FontName = "Calibri";
+            text.Width = 100;
+            text.Copy();
+            Console.ReadLine();
+        }
+    }
+}
