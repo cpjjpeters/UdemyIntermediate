4c295254e9c467caa760127bf264065ee3e72add
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2018-07-16 20:18:46 +0200

    new stuff

---
+++ MultipleInheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace MultipleInheritance
+{
+    public class TextBox : UiControl, IDraggable, IDroppable
+    {
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
